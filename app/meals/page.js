79030c23b0d8518d47dc186456616c56e7cf7c33
Dominik Speidel <dominik-speidel@posteo.de>
2024-08-05 06:20:43 +0200

    meal-item and meal-grid component implemented

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,23 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+
 export default function MealsPage() {
-  return <h1>Share Meals</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+          <p>
+            Coose your favorrite recipe and cook it yourself. It is easy and fun
+          </p>
+          <p className={classes.cta}>
+            <Link href="/meals/share">Share Your Favorite Recipe</Link>
+          </p>
+        </h1>
+      </header>
+
+      <main className={classes.main}></main>
+    </>
+  );
 }
