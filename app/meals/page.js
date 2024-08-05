2bf329a82c2c0f476f606f2a98ef9fb3837d99e3
Dominik Speidel <dominik-speidel@posteo.de>
2024-08-05 11:45:31 +0200

    SQLite Database implemented

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,16 @@
 import Link from "next/link";
 import classes from "./page.module.css";
+import MealsGrid from "@/Components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
 export default function MealsPage() {
+  const meals = getMeals();
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Delicious meals, created{" "}
-          <span className={classes.highlight}>by you</span>
+          Delicious meals, created
+          <span className={classes.highlight}> by you</span>
           <p>
             Coose your favorrite recipe and cook it yourself. It is easy and fun
           </p>
@@ -17,7 +20,9 @@ export default function MealsPage() {
         </h1>
       </header>
 
-      <main className={classes.main}></main>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
     </>
   );
 }
